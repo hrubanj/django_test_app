7e7b825eac8ec7359fd52d0eb40a20d9448da5a9
hrubanj <jiri.hruban@email.cz>
2020-05-18 07:41:45 +0200

    fixed url in finc tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -13,7 +13,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.browser = webdriver.Firefox()
         staging_server = os.environ.get('STAGING_SERVER')
         if staging_server:
-            self.live_server_url = 'http://' + staging_server
+            self.live_server_url = 'http://' + 'jirihruban.pythonanywhere.com'
 
 
     def tearDown(self):
